948fdfb0f254f1b5dc2eaf6074e1070a4dd458ab
unknown <vishalsindhu@aim.com>
2019-03-05 17:00:42 -0500

    error

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('b1');
+INSERT INTO burgers (name) VALUES ('B2');
+INSERT INTO burgers (name, devour) VALUES ('b3', true);
+INSERT INTO burgers (name, devour) VALUES ('b4', true);
+INSERT INTO burgers (name, devour) VALUES ('b5', true);
+INSERT INTO burgers (name) VALUES ('b6');
